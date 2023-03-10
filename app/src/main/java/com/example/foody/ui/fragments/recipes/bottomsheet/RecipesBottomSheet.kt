9ad3876193d8a113f5cf42cea33cf02cd567cf0a
d.sauerbronn <linkdayan2000@gmail.com>
2023-03-09 19:12:35 -0600

    adding on change bits

--- app/src/main/java/com/example/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -1,21 +1,80 @@
 package com.example.foody.ui.fragments.recipes.bottomsheet
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import com.example.foody.R
+import com.example.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.foody.viewmodles.RecipesViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import kotlinx.android.synthetic.main.recipes_bottom_sheet.view.*
+import java.util.*
 
 
 class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    private lateinit var recipesViewModel: RecipesViewModel
+    private var mealTypeChip = DEFAULT_MEAL_TYPE
+    private var mealTypeChipId = 0
+    private var dietTypeChip = DEFAULT_DIET_TYPE
+    private var dietTypeChipId = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+        val mView = inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+
+        recipesViewModel.readMealAndDietType.asLiveData().observe(viewLifecycleOwner) { values ->
+            mealTypeChip = values.selectedMealType
+            dietTypeChip = values.selectedDietType
+            updateChip(values.selectedDietTypeId, mView.mealType_chipGroup)
+            updateChip(values.selectedMealTypId, mView.dietType_chipGroup)
+        }
+
+        mView.mealType_chipGroup.setOnCheckedChangeListener { group, selectedChipId ->
+            mealTypeChip =
+                group.findViewById<Chip>(selectedChipId).text.toString().toLowerCase(Locale.ROOT)
+            mealTypeChipId = selectedChipId
+        }
+
+        mView.dietType_chipGroup.setOnCheckedChangeListener { group, selectedChipId ->
+            dietTypeChip =
+                group.findViewById<Chip>(selectedChipId).text.toString().toLowerCase(Locale.ROOT)
+            dietTypeChipId = selectedChipId
+        }
+
+        mView.apply_btn.setOnClickListener {
+            recipesViewModel.saveMealAndDietType(
+                mealTypeChip,
+                mealTypeChipId,
+                dietTypeChip,
+                dietTypeChipId
+            )
+        }
+        return mView
+    }
+
+    private fun updateChip(chipId: Int, chipGroup: ChipGroup) {
+        if (chipId != 0) {
+            try {
+                chipGroup.findViewById<Chip>(chipId).isChecked = true
+            } catch (e: Exception) {
+                Log.d("RecipesBottomSheet", e.message.toString())
+            }
+        }
     }
 
 }
